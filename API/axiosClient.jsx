4232587281837408e3db8507696e3d14ddfeeb47
dobby30102001-cpu <dobby30102001@gmail.com>
2026-01-29 11:37:41 +0900

    Restore axios client setup and response interceptor
    Uncomment axios client configuration and interceptor.

--- API/axiosClient.jsx
+++ API/axiosClient.jsx
@@ -1,18 +1,4 @@
-// import axios from "axios";
 
-// const axiosClient = axios.create({
-//     baseURL: "http://localhost:8080/api/auth",
-// });
-
-// axiosClient.interceptors.response.use(
-//     (res) => res,
-//     (err) => {
-//         console.error(err);
-//         throw err;
-//     }
-// );
-
-// export default axiosClient;
 
 import axios from "axios";
 
